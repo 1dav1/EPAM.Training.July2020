e1059ed6d5e63294485bab5b0526ab93f1a50ee2
AnatoliDzenisenka <notif@tut.by>
2020-07-10 23:47:57 +0300

    Initial commit

---
+++ GCDClassLibrary/GCDClassLibrary/EuclidianAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCDClassLibrary
+{
+    public class EuclidianAlgorithm
+    {
+        public int CalculateGCD(int number1, int number2)
+        {
+            if (number2 == 0)
+                return number1;
+            return CalculateGCD(number2, number1 % number2);
+        }
+    }
+}
