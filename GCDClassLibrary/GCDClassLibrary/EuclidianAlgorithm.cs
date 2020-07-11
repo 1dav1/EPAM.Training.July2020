d2886c81298edf95c4254a6da6e175b2bba28262
AnatoliDzenisenka <notif@tut.by>
2020-07-11 11:04:10 +0300

    Add overloaded methods

--- GCDClassLibrary/GCDClassLibrary/EuclidianAlgorithm.cs
+++ GCDClassLibrary/GCDClassLibrary/EuclidianAlgorithm.cs
@@ -15,5 +15,47 @@
                 return CalculateGCD(number2, number1 % number2);
             }
         }
+
+        public int CalculateGCD(int number1, int number2, int number3)
+        {
+            if (number1 < 0 || number2 < 0 || number3 < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int midResult = CalculateGCD(number1, number2);
+                return CalculateGCD(midResult, number3);
+            }
+        }
+
+        public int CalculateGCD(int number1, int number2, int number3, int number4)
+        {
+            if (number1 < 0 || number2 < 0 || number3 < 0 || number4 < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int midResult = CalculateGCD(number1, number2);
+                midResult = CalculateGCD(midResult, number3);
+                return CalculateGCD(midResult, number4);
+            }
+        }
+
+        public int CalculateGCD(int number1, int number2, int number3, int number4, int number5)
+        {
+            if (number1 < 0 || number2 < 0 || number3 < 0 || number4 < 0 || number5 < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int midResult = CalculateGCD(number1, number2);
+                midResult = CalculateGCD(midResult, number3);
+                midResult = CalculateGCD(midResult, number4);
+                return CalculateGCD(midResult, number5);
+            }
+        }
     }
 }
