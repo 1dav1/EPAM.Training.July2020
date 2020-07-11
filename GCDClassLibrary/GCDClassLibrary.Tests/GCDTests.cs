8d43fe665107c37da7e2823dd856d3a156bf77bc
AnatoliDzenisenka <notif@tut.by>
2020-07-11 15:59:56 +0300

    Rename Class, add tests

---
+++ GCDClassLibrary/GCDClassLibrary.Tests/GCDTests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace GCDClassLibrary.Tests
+{
+    public class GCDTests
+    {
+        private const int NUMBER1 = 252;
+        private const int NUMBER2 = 105;
+        private const int NUMBER3 = 147;
+        private const int NUMBER4 = 63;
+        private const int NUMBER5 = 642;
+
+        private const int RESULT = 21;
+
+        [Fact]
+        public void CalculateGCD_WhenArgumentsAreValid_ShouldReturnCorrectValue()
+        {
+            // Arrange - Act
+            GCDCalculator gcdCalculator = new GCDCalculator();
+
+            // Assert
+            gcdCalculator.CalculateGCD(NUMBER1, NUMBER2)
+                .Should()
+                .Equals(RESULT);
+
+            gcdCalculator.CalculateGCD(NUMBER1, NUMBER2, NUMBER3)
+                .Should()
+                .Equals(RESULT);
+
+            gcdCalculator.CalculateGCD(NUMBER1, NUMBER2, NUMBER3, NUMBER4)
+                .Should()
+                .Equals(RESULT);
+
+            gcdCalculator.CalculateGCD(NUMBER1, NUMBER2, NUMBER3, NUMBER4, NUMBER5)
+                .Should()
+                .Equals(RESULT);
+        }
+
+        [Fact]
+        public void CalculateBinaryGCD_WhenArgumentsAreValid_ShouldReturnCorrectValue()
+        {
+            // Arrange - Act
+            GCDCalculator gcdCalculator = new GCDCalculator();
+            //int result = 0;
+            //DateTime endTime;
+            //DateTime begTime = DateTime.Now;
+
+            //endTime = DateTime.Now;
+
+            // Assert
+            gcdCalculator.CalculateBinaryGCD(out _, NUMBER1, NUMBER2)
+                .Should()
+                .Equals(RESULT);
+        }
+
+        //[Fact]
+        //public void CalculateGCD_WhenArgumentsAreValid_ShouldOutputCorrectTime()
+        //{
+        //    // Arrange
+        //    GCDCalculator gcdCalculator = new GCDCalculator();
+        //    DateTime endTime;
+        //    DateTime begTime;
+
+        //    // Act
+        //    begTime = DateTime.Now;
+        //    _ = gcdCalculator.CalculateGCD(out double time, NUMBER1, NUMBER2);
+        //    endTime = DateTime.Now;
+
+        //    // Assert
+        //    time
+        //        .Should()
+        //        .BeApproximately((endTime - begTime).TotalMilliseconds, 0.1f);
+        //}
+
+        [Fact]
+        public void GetHistogram_ShouldReturnCorrectValue()
+        {
+            // Arrange - Act
+            GCDCalculator gcdCalculator = new GCDCalculator();
+
+            // Assert
+            gcdCalculator.GetHistogram()
+                .Should()
+                .NotBeEmpty()
+                .And.ContainKeys("GCD", "BinaryGCD");
+        }
+    }
+}
--- GCDClassLibrary/GCDClassLibrary/GCDCalculator.cs
+++ GCDClassLibrary/GCDClassLibrary/GCDCalculator.cs
@@ -1,20 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GCDClassLibrary
 {
-    public class EuclidianAlgorithm
+    public class GCDCalculator
     {
         private double GCDTime { get; set; }
 
         private double BinaryGCDTime { get; set; }
 
-        public int CalculateGCD(int number1, int number2)
+        // multiplier to convert milliseconds to pixels
+        private const int MULTIPLIER = 1000;
+
+        public uint CalculateGCD(uint number1, uint number2)
         {
-            if (number1 < 0 || number2 < 0)
-            {
-                return 0;
-            }
-            else if (number1 == number2)
+            //if (number1 < 0 || number2 < 0)
+            //{
+            //    return 0;
+            //}
+            if (number1 == number2)
             {
                 return number1;
             }
@@ -31,38 +35,24 @@ namespace GCDClassLibrary
 
         public int CalculateGCD(out double time, int number1, int number2)
         {
-            DateTime endTime;
             DateTime begTime = DateTime.Now;
-
-            if (number1 < 0 || number2 < 0)
+            while(number1 != number2)
             {
-                endTime = DateTime.Now;
-                time = (endTime - begTime).TotalMilliseconds;
-                GCDTime = time;
-                return 0;
-            }
-            else if (number1 == number2)
-            {
-                endTime = DateTime.Now;
-                time = (endTime - begTime).TotalMilliseconds;
-                GCDTime = time;
-                return number1;
-            }
-            else
-            {
-                if (number2 == 0)
+                if (number1 > number2)
                 {
-                    endTime = DateTime.Now;
-                    time = (endTime - begTime).TotalMilliseconds;
-                    GCDTime = time;
-                    return number1;
+                    number1 -= number2;
                 }
-
-                return CalculateGCD(out time, number2, number1 % number2);
+                else
+                {
+                    number2 -= number1;
+                }
             }
+            DateTime endTime = DateTime.Now;
+            time = (endTime - begTime).TotalMilliseconds;
+            return number1;
         }
 
-        public int CalculateGCD(int number1, int number2, int number3)
+        public uint CalculateGCD(uint number1, uint number2, uint number3)
         {
             if (number1 < 0 || number2 < 0 || number3 < 0)
             {
@@ -74,12 +64,12 @@ namespace GCDClassLibrary
             }
             else
             {
-                int midResult = CalculateGCD(number1, number2);
+                uint midResult = CalculateGCD(number1, number2);
                 return CalculateGCD(midResult, number3);
             }
         }
 
-        public int CalculateGCD(int number1, int number2, int number3, int number4)
+        public uint CalculateGCD(uint number1, uint number2, uint number3, uint number4)
         {
             if (number1 < 0 || number2 < 0 || number3 < 0 || number4 < 0)
             {
@@ -91,13 +81,13 @@ namespace GCDClassLibrary
             }
             else
             {
-                int midResult = CalculateGCD(number1, number2);
+                uint midResult = CalculateGCD(number1, number2);
                 midResult = CalculateGCD(midResult, number3);
                 return CalculateGCD(midResult, number4);
             }
         }
-
-        public int CalculateGCD(int number1, int number2, int number3, int number4, int number5)
+        
+        public uint CalculateGCD(uint number1, uint number2, uint number3, uint number4, uint number5)
         {
             if (number1 < 0 || number2 < 0 || number3 < 0 || number4 < 0 || number5 < 0)
             {
@@ -109,7 +99,7 @@ namespace GCDClassLibrary
             }
             else
             {
-                int midResult = CalculateGCD(number1, number2);
+                uint midResult = CalculateGCD(number1, number2);
                 midResult = CalculateGCD(midResult, number3);
                 midResult = CalculateGCD(midResult, number4);
                 return CalculateGCD(midResult, number5);
@@ -157,7 +147,7 @@ namespace GCDClassLibrary
                 }
             }
 
-            if (number2 % 2 == 0)
+            if ((number2 & 1) == 0)
             {
                 return CalculateBinaryGCD(out time, number1, number2 >> 1);
             }
@@ -168,6 +158,14 @@ namespace GCDClassLibrary
             }
 
             return CalculateBinaryGCD(out time, (number2 - number1) >> 1, number1);
+        }
+
+        public SortedDictionary<string, double> GetHistogram()
+        {
+            SortedDictionary<string, double> histogram = new SortedDictionary<string, double>();
+            histogram.Add("GCD", GCDTime * MULTIPLIER);
+            histogram.Add("BinaryGCD", BinaryGCDTime * MULTIPLIER);
+            return histogram;
         }
     }
 }
