efa5e238d0c775a32da6c1980edf442950155259
AnatoliDzenisenka <notif@tut.by>
2020-07-11 10:46:18 +0300

    Add conditional statement to operate on negatives

--- GCDClassLibrary/GCDClassLibrary/EuclidianAlgorithm.cs
+++ GCDClassLibrary/GCDClassLibrary/EuclidianAlgorithm.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GCDClassLibrary
+﻿namespace GCDClassLibrary
 {
     public class EuclidianAlgorithm
     {
         public int CalculateGCD(int number1, int number2)
         {
-            if (number2 == 0)
-                return number1;
-            return CalculateGCD(number2, number1 % number2);
+            if (number1 < 0 || number2 < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                if (number2 == 0)
+                    return number1;
+                return CalculateGCD(number2, number1 % number2);
+            }
         }
     }
 }
