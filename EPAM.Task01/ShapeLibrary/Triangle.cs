2e116e7a9942a135e4b05f57bd2c18a2248c0628
AnatoliDzenisenka <notif@tut.by>
2020-07-16 21:21:47 +0300

    XML comments added, Triangle class amended

--- EPAM.Task01/ShapeLibrary/Triangle.cs
+++ EPAM.Task01/ShapeLibrary/Triangle.cs
@@ -2,12 +2,19 @@
 
 namespace ShapeLibrary
 {
+    /// <include file='docs.xml' path='docs/members[@name="triangle"]/Triangle/*'/>
     public class Triangle : Shape
     {
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/Side1/*'/>
         public double Side1 { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/Side2/*'/>
         public double Side2 { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/Side3/*'/>
         public double Side3 { get; set; }
 
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/GetPerimeter/*'/>
         public override double GetPerimeter()
         {
             if (Side1 < 0 || Side2 < 0 || Side3 < 0)
@@ -15,32 +22,41 @@ namespace ShapeLibrary
             return Side1 + Side2 + Side3;
         }
 
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/GetArea/*'/>
         public override double GetArea()
         {
-            if(Side1 < 0 || Side2 < 0 || Side3 < 0)
+            if (Side1 < 0 || Side2 < 0 || Side3 < 0)
                 throw new ArgumentOutOfRangeException("Side", "Sides must be positive.");
 
             double perimeter = GetPerimeter();
             return Math.Sqrt(perimeter * (perimeter - Side1) * (perimeter - Side2) * (perimeter - Side3));
         }
 
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/ToString/*'/>
         public override string ToString()
         {
             return $"Triangle. Side 1 = {Side1}; Side 2 = {Side2}; Side 3 = {Side3}";
         }
 
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/Equals/*'/>
         public override bool Equals(object obj)
         {
-            if (GetType() != obj.GetType())
-                return false;
+            // using SSS comparison to determine the congruence 
+            return obj is Triangle triangle &&
+                    ((Side1 == triangle.Side1 &&
+                      Side2 == triangle.Side2 &&
+                      Side3 == triangle.Side3) ||
 
-            Triangle triangle = (Triangle)obj; 
+                     (Side1 == triangle.Side2 &&
+                      Side2 == triangle.Side3 &&
+                      Side3 == triangle.Side1) ||
 
-            return (Side1 == triangle.Side1 && 
-                    Side2 == triangle.Side2 && 
-                    Side3 == triangle.Side3);
+                     (Side1 == triangle.Side3 &&
+                      Side2 == triangle.Side1 &&
+                      Side3 == triangle.Side2));
         }
 
+        /// <include file='docs.xml' path='docs/members[@name="triangle"]/GetHashCode/*'/>
         public override int GetHashCode()
         {
             return HashCode.Combine(Side1, Side2, Side3);
