c601b8eeaebd6f6a921a96a965e2c485fd16ae08
AnatoliDzenisenka <notif@tut.by>
2020-07-16 21:08:02 +0300

    XML comments added, base class Shape amended

--- EPAM.Task01/ShapeLibrary/Shape.cs
+++ EPAM.Task01/ShapeLibrary/Shape.cs
@@ -1,8 +1,12 @@
 ﻿namespace ShapeLibrary
 {
+    /// <include file='docs.xml' path='docs/members[@name="shape"]/Shape/*'/>
     public abstract class Shape
     {
-        public abstract double GetArea();
+        /// <include file='docs.xml' path='docs/members[@name="shape"]/GetPerimeter/*'/>
         public abstract double GetPerimeter();
+
+        /// <include file='docs.xml' path='docs/members[@name="shape"]/GetArea/*'/>
+        public abstract double GetArea();
     }
 }
