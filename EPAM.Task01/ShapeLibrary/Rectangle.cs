621f50bbc756e2df24ea2212ae29841d53558c54
AnatoliDzenisenka <notif@tut.by>
2020-07-16 21:01:31 +0300

    XML comments added, class Rectangle amended.

--- EPAM.Task01/ShapeLibrary/Rectangle.cs
+++ EPAM.Task01/ShapeLibrary/Rectangle.cs
@@ -2,11 +2,25 @@
 
 namespace ShapeLibrary
 {
+    /// <include file='docs.xml' path='docs/members[@name="rectangle"]/Rectangle/*'/>
     public class Rectangle : Shape
     {
+        /// <include file='docs.xml' path='docs/members[@name="rectangle"]/Height/*'/>
         public double Height { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="rectangle"]/Width/*'/>
         public double Width { get; set; }
 
+        /// <include file='docs.xml' path='docs/members[@name="rectangle"]/GetPerimeter/*'/>
+        public override double GetPerimeter()
+        {
+            if (Height < 0 || Width < 0)
+                throw new ArgumentOutOfRangeException("Side", "Sides must be positive.");
+
+            return (Height + Width) * 2;
+        }
+
+        /// <include file='docs.xml' path='docs/members[@name="rectangle"]/GetArea/*'/>
         public override double GetArea()
         {
             if (Height < 0 || Width < 0)
@@ -15,30 +29,21 @@ namespace ShapeLibrary
             return Height * Width;
         }
 
-        public override double GetPerimeter()
-        {
-            if (Height < 0 || Width < 0)
-                throw new ArgumentOutOfRangeException("Side", "Sides must be positive.");
-            return (Height + Width) * 2;
-        }
-
+        /// <include file='docs.xml' path='docs/members[@name="rectangle"]/ToString/*'/>
         public override string ToString()
         {
             return $"Rectangle. Height = {Height}; Width = {Width}";
         }
 
+        /// <include file='docs.xml' path='docs/members[@name="rectangle"]/Equals/*'/>
         public override bool Equals(object obj)
         {
-            if (GetType() != obj.GetType())
-                return false;
-
-            Rectangle rectangle = (Rectangle)obj;
-
-            return (Height == rectangle.Height &&
-                    GetArea() == rectangle.GetArea() &&
-                    Width == rectangle.Width);
+            return obj is Rectangle rectangle &&
+                   Height == rectangle.Height &&
+                   Width == rectangle.Width;
         }
 
+        /// <include file='docs.xml' path='docs/members[@name="rectangle"]/GetHashCode/*'/>
         public override int GetHashCode()
         {
             return HashCode.Combine(Height, Width);
