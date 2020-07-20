7b0cb70dc90d94ae2bc6718c2436aed16766fa6a
AnatoliDzenisenka <notif@tut.by>
2020-07-20 22:15:46 +0300

    Add class library for subtask 3.

---
+++ EPAM.Task02/src/ProductClassLibrary/Clothing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductClassLibrary
+{
+    class Clothing : Product
+    {
+        public override string Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override double Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ EPAM.Task02/src/ProductClassLibrary/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProductClassLibrary
+{
+    public abstract class Product
+    {
+        public abstract string Type { get; set; }
+        public abstract string Name { get; set; }
+        public abstract double Price { get; set; }
+    }
+}
