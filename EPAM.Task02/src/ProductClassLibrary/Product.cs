8657203c459e707de06502d0c0326258b2a489e1
AnatoliDzenisenka <notif@tut.by>
2020-07-21 22:03:15 +0300

    Minor amendments.

--- EPAM.Task02/src/ProductClassLibrary/Product.cs
+++ EPAM.Task02/src/ProductClassLibrary/Product.cs
@@ -10,9 +10,5 @@ namespace ProductClassLibrary
 
         /// <include file='docs.xml' path='docs/members[@name="product"]/Price/*'/>
         public abstract decimal Price { get; set; }
-
-        //public abstract override bool Equals(object obj);
-
-        //public abstract override int GetHashCode();
     }
 }
