929f0825054ccff4a3e36e6e91adda3834d3d281
AnatoliDzenisenka <notif@tut.by>
2020-07-25 14:32:02 +0300

    Add interfaces

---
+++ EPAM.Task03/src/ShapeClassLibrary/Interfaces/IPaper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ShapeClassLibrary
+{
+    public enum Colors
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Black,
+        White,
+        Grey,
+    }
+    public interface IPaper
+    {
+        public Colors Color { get; set; }
+    }
+}
