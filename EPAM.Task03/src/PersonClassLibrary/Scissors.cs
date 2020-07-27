3242653449e7762319cc9bde67ab5b4b2fbff9c8
AnatoliDzenisenka <notif@tut.by>
2020-07-27 18:52:42 +0300

    Change class to static

--- EPAM.Task03/src/PersonClassLibrary/Scissors.cs
+++ EPAM.Task03/src/PersonClassLibrary/Scissors.cs
@@ -3,37 +3,37 @@ using System;
 
 namespace PersonClassLibrary
 {
-    public class Scissors
+    public static class Scissors
     {
-        public Shape Cut(IPaper paper, params double[] parameters)
+        public static Shape Cut(IPaper paper, params double[] parameters)
         {
             if (paper is null)
                 throw new ArgumentNullException();
 
             return parameters.Length switch
             {
-                1 => new PaperCircle(parameters),
-                2 => new PaperRectangle(parameters),
-                3 => new PaperTriangle(parameters),
+                1 => new PaperCircle(parameters[0]),
+                2 => new PaperRectangle(parameters[0], parameters[1]),
+                3 => new PaperTriangle(parameters[0], parameters[1], parameters[2]),
                 _ => throw new ArgumentOutOfRangeException("Wrong number of parameters."),
             };
         }
 
-        public Shape Cut(IFilm film, params double[] parameters)
+        public static Shape Cut(IFilm film, params double[] parameters)
         {
             if (film is null)
                 throw new ArgumentNullException();
 
             return parameters.Length switch
             {
-                1 => new FilmCircle(parameters),
-                2 => new FilmRectangle(parameters),
-                3 => new FilmTriangle(parameters),
+                1 => new FilmCircle(parameters[0]),
+                2 => new FilmRectangle(parameters[0], parameters[1]),
+                3 => new FilmTriangle(parameters[0], parameters[1], parameters[2]),
                 _ => throw new ArgumentOutOfRangeException("Wrong number of parameters."),
             };
         }
 
-        public Shape Cut(Shape shape, params double[] parameters)
+        public static Shape Cut(Shape shape, params double[] parameters)
         {
             if (shape is null)
                 throw new ArgumentNullException();
@@ -42,9 +42,9 @@ namespace PersonClassLibrary
             {
                 return parameters.Length switch
                 {
-                    1 => new PaperCircle(shape, parameters),
-                    2 => new PaperRectangle(shape, parameters),
-                    3 => new PaperTriangle(shape, parameters),
+                    1 => new PaperCircle(shape, parameters[0]),
+                    2 => new PaperRectangle(shape, parameters[0], parameters[1]),
+                    3 => new PaperTriangle(shape, parameters[0], parameters[1], parameters[2]),
                     _ => throw new ArgumentOutOfRangeException("Wrong number of parameters."),
                 };
             }
@@ -53,9 +53,9 @@ namespace PersonClassLibrary
             {
                 return parameters.Length switch
                 {
-                    1 => new FilmCircle(shape, parameters),
-                    2 => new FilmRectangle(shape, parameters),
-                    3 => new FilmTriangle(shape, parameters),
+                    1 => new FilmCircle(shape, parameters[0]),
+                    2 => new FilmRectangle(shape, parameters[0], parameters[1]),
+                    3 => new FilmTriangle(shape, parameters[0], parameters[1], parameters[2]),
                     _ => throw new ArgumentOutOfRangeException("Wrong number of parameters."),
                 };
             }
