84761d7b8d2086584f2a0b30e8f3a760f4c0193a
AnatoliDzenisenka <notif@tut.by>
2020-08-03 18:44:02 +0300

    Add XML comments

--- EPAM.Task04/src/StateClassLibrary/MessageReceivedEventArgs.cs
+++ EPAM.Task04/src/StateClassLibrary/MessageReceivedEventArgs.cs
@@ -4,9 +4,13 @@ using System.Net;
 namespace StateClassLibrary
 {
     // information about the event
+    /// <include file='docs.xml' path='docs/members[@name="args"]/Args/*'/>
     public class MessageReceivedEventArgs : EventArgs
     {
+        /// <include file='docs.xml' path='docs/members[@name="args"]/EndPoint/*'/>
         public EndPoint EndPoint { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="args"]/Message/*'/>
         public string Message { get; set; }
     }
 }
--- EPAM.Task04/src/StateClassLibrary/State.cs
+++ EPAM.Task04/src/StateClassLibrary/State.cs
@@ -4,12 +4,22 @@ using System.Text;
 namespace StateClassLibrary
 {
     // state object for storing data and passing it to callback functions at asynchronous server and client
+    /// <include file='docs.xml' path='docs/members[@name="state"]/State/*'/>
     public class State
     {
+        /// <include file='docs.xml' path='docs/members[@name="state"]/State/*'/>
         public Socket Socket { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="state"]/BufferSize/*'/>
         public int BufferSize { get; private set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="state"]/Buffer/*'/>
         public byte[] Buffer { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="state"]/StringBuilder/*'/>
         public StringBuilder StringBuilder { get; set; }
+
+        /// <include file='docs.xml' path='docs/members[@name="state"]/Constructor/*'/>
         public State(int bufferSize = 1024)
         {
             BufferSize = bufferSize;
