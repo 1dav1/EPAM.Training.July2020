379a52ce96b44bf9708c44e170e8f601104d1b8a
AnatoliDzenisenka <notif@tut.by>
2020-08-16 01:44:54 +0300

    Add XML comments

--- EPAM.Task05/src/CustomSerializerClassLibrary/Interfaces/ISerialize.cs
+++ EPAM.Task05/src/CustomSerializerClassLibrary/Interfaces/ISerialize.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace CustomSerializerClassLibrary.Interfaces
 {
+    /// <include file='docs.xml' path='docs/members[@name="iserialize"]/ISerialize/*'/>
     public interface ISerialize : ISerializable
     {
     }
