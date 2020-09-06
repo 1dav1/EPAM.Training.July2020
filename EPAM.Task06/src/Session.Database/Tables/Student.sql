de2e77fd71c340f46a3e06d9ec599195f850039b
AnatoliDzenisenka <notif@tut.by>
2020-09-06 21:37:29 +0300

    Add database project

---
+++ EPAM.Task06/src/Session.Database/Tables/Student.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (20) NOT NULL,
+    [Gender]    NVARCHAR (20) NOT NULL,
+    [BirthDate] DATE          NOT NULL,
+    [GroupId]   INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
