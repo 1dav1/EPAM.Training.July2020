c04d5dbe7a24f0b2609eb6f5f3e8bb7c6ace5dc7
AnatoliDzenisenka <notif@tut.by>
2020-09-06 23:39:40 +0300

    Add foreign key

---
+++ EPAM.Task06/src/Session.Database/ForeignKeys/ForeignKey_Assessment_Group.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[Assessment]
+	ADD CONSTRAINT  [ForeignKey_Assessment_Group] 
+	FOREIGN KEY ([GroupId]) 
+	REFERENCES [dbo].[StudentGroup] ([Id])
--- EPAM.Task06/src/Session.Database/ForeignKeys/ForeignKey_Grade_Student.sql
+++ EPAM.Task06/src/Session.Database/ForeignKeys/ForeignKey_Grade_Student.sql
@@ -1,4 +1,4 @@
 ﻿ALTER TABLE [dbo].[Grade]
-	ADD CONSTRAINT  [ForeignKey_Grade_Assessment] 
+	ADD CONSTRAINT  [ForeignKey_Grade_Student] 
 	FOREIGN KEY ([StudentId]) 
 	REFERENCES [dbo].[Student] ([Id])
