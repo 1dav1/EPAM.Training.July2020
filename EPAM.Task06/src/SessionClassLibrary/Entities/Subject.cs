8978c37fc0aa0bdfaa75fedb730a5b2b4fefcaab
AnatoliDzenisenka <notif@tut.by>
2020-09-07 20:41:05 +0300

    Amend class

--- EPAM.Task06/src/SessionClassLibrary/Entities/Subject.cs
+++ EPAM.Task06/src/SessionClassLibrary/Entities/Subject.cs
@@ -1,8 +1,47 @@
-﻿namespace SessionClassLibrary
+﻿using System;
+
+namespace SessionClassLibrary
 {
     public class Subject
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("ID should be positive");
+                _id = value;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value is null || value == "")
+                    throw new ArgumentOutOfRangeException("The name should not be empty.");
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            return obj is Subject subject &&
+                   subject.Id == Id &&
+                   subject.Name == Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
     }
 }
