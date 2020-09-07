6dc7c2d217685e3aaf9c295ef045a00ea426fa19
AnatoliDzenisenka <notif@tut.by>
2020-09-07 20:40:57 +0300

    Amend class

--- EPAM.Task06/src/SessionClassLibrary/Entities/StudentGroup.cs
+++ EPAM.Task06/src/SessionClassLibrary/Entities/StudentGroup.cs
@@ -1,8 +1,46 @@
-﻿namespace SessionClassLibrary
+﻿using System;
+
+namespace SessionClassLibrary
 {
     public class StudentGroup
     {
-        public int Id { get; set; }
-        public string Number { get; set; }
+        private int _id; 
+        public int Id { get => _id;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("ID should be positive");
+                _id = value;
+            }
+        }
+
+        private string _number;
+        public string Number 
+        { 
+            get => _number; 
+            set
+            {
+                if (value == string.Empty || value is null)
+                    throw new ArgumentOutOfRangeException("The group number should not be empty.");
+                _number = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            return obj is StudentGroup group &&
+                   group.Id == Id &&
+                   group.Number == Number;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Number);
+        }
     }
 }
