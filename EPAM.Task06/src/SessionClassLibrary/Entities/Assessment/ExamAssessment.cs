4b5b5f1cb6d8124a687223a4f3805175650f7071
AnatoliDzenisenka <notif@tut.by>
2020-09-07 20:40:01 +0300

    Amend class

--- EPAM.Task06/src/SessionClassLibrary/Entities/Assessment/ExamAssessment.cs
+++ EPAM.Task06/src/SessionClassLibrary/Entities/Assessment/ExamAssessment.cs
@@ -6,10 +6,74 @@ namespace SessionClassLibrary.Entities.Assessment
 {
     public class ExamAssessment : Assessment
     {
-        public override int Id { get; set; }
+        private int _id;
+        public override int Id 
+        { 
+            get => _id;
+            set 
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("ID should be positive.");
+                _id = value;
+            }
+        }
+
         public override DateTime Date { get; set; }
-        public override int NumberOfSession { get; set; }
-        public override int SubjectId { get; set; }
-        public override int GroupId { get; set; }
+
+        private int _numberOfSession;
+        public override int NumberOfSession 
+        { 
+            get => _numberOfSession;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Number of the session should be positive.");
+                _numberOfSession = value;
+            } 
+        }
+
+        private int _subjectId;
+        public override int SubjectId 
+        { 
+            get => _subjectId;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Subject ID should be positive.");
+                _subjectId = value;
+            } 
+        }
+
+        private int _groupId;
+        public override int GroupId 
+        { 
+            get => _groupId; 
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Group ID should be positive");
+                _groupId = value;
+            } 
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            return obj is ExamAssessment exam &&
+                   exam.Id == Id &&
+                   exam.SubjectId == SubjectId &&
+                   exam.GroupId == GroupId &&
+                   exam.Date == Date &&
+                   exam.NumberOfSession == NumberOfSession;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Date, NumberOfSession, SubjectId, GroupId);
+        }
     }
 }
