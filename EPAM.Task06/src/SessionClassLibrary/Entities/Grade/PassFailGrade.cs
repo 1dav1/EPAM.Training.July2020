1331acf503ac1d52486d628a9da231f242807d59
AnatoliDzenisenka <notif@tut.by>
2020-09-07 20:40:24 +0300

    Amend class

--- EPAM.Task06/src/SessionClassLibrary/Entities/Grade/PassFailGrade.cs
+++ EPAM.Task06/src/SessionClassLibrary/Entities/Grade/PassFailGrade.cs
@@ -4,9 +4,40 @@ namespace SessionClassLibrary.Entities.Grade
 {
     public class PassFailGrade : Grade
     {
-        public override int Id { get; set; }
-        public override int AssessmentId { get; set; }
-        public override int StudentId { get; set; }
+        private int _id;
+        public override int Id
+        {
+            get => _id;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("ID should be positive.");
+                _id = value;
+            }
+        }
+
+        private int _assessmentId;
+        public override int AssessmentId 
+        {
+            get => _assessmentId;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Assessment ID should be positive.");
+            } 
+        }
+
+        private int _studentId;
+        public override int StudentId 
+        { 
+            get => _studentId;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Student ID should be positive");
+                _studentId = value;
+            } 
+        }
 
         private string _value;
         public string Value
@@ -15,9 +46,28 @@ namespace SessionClassLibrary.Entities.Grade
             set
             {
                 if (value != "Pass" && value != "Fail")
-                    throw new Exception("Wrong value. Grade should be 'Pass' or 'Fail'.");
+                    throw new ArgumentOutOfRangeException("Wrong value. Grade should be 'Pass' or 'Fail'.");
                 _value = value;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            return obj is PassFailGrade grade &&
+                   grade.Id == Id &&
+                   grade.StudentId == StudentId &&
+                   grade.AssessmentId == AssessmentId &&
+                   grade.Value == Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, AssessmentId, StudentId, Value);
         }
     }
 }
