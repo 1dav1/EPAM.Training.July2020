beacc8194437d49f8a212fe908dff6818fdaaa44
AnatoliDzenisenka <notif@tut.by>
2020-09-07 20:46:00 +0300

    Amend data for database

--- EPAM.Task06/src/Session.Database/Data/Data.sql
+++ EPAM.Task06/src/Session.Database/Data/Data.sql
@@ -1,4 +1,5 @@
-﻿INSERT INTO [dbo].[StudentGroup] 
+﻿USE [SessionDB]
+INSERT INTO [dbo].[StudentGroup] 
 			(Number) 
 VALUES ('G1'),
 	   ('G2'),
@@ -40,28 +41,28 @@ VALUES ('2018-08-30', 1, 1, 1, 'Test'),
 
 INSERT INTO [dbo].[Grade]
 (AssessmentId, StudentId, Value, GradeType)
-VALUES (1, 2, 'Pass', 'PassFail'),
-	   (1, 3, 'Pass', 'PassFail'),
-	   (1, 4, 'Fail', 'PassFail'),
+VALUES (1, 1, 'Pass', 'PassFail'),
+	   (1, 2, 'Pass', 'PassFail'),
+	   (1, 3, 'Fail', 'PassFail'),
+	   (2, 1, 'Pass', 'PassFail'),
 	   (2, 2, 'Pass', 'PassFail'),
 	   (2, 3, 'Pass', 'PassFail'),
-	   (2, 4, 'Pass', 'PassFail'),
+	   (3, 1, 'Pass', 'PassFail'),
 	   (3, 2, 'Pass', 'PassFail'),
-	   (3, 3, 'Pass', 'PassFail'),
-	   (4, 5, '9', 'Point'),
-	   (4, 6, '8', 'Point'),
-	   (4, 7, '7', 'Point'),
-	   (5, 5, 'Fail', 'PassFail'),
+	   (4, 4, '9', 'Point'),
+	   (4, 5, '8', 'Point'),
+	   (4, 6, '7', 'Point'),
+	   (5, 4, 'Fail', 'PassFail'),
+	   (5, 5, 'Pass', 'PassFail'),
 	   (5, 6, 'Pass', 'PassFail'),
-	   (5, 7, 'Pass', 'PassFail'),
-	   (6, 8, '6', 'Point'),
+	   (6, 7, '6', 'Point'),
+	   (6, 8, '9', 'Point'),
 	   (6, 9, '9', 'Point'),
-	   (6, 10, '9', 'Point'),
+	   (7, 7, '7', 'Point'),
 	   (7, 8, '7', 'Point'),
 	   (7, 9, '7', 'Point'),
-	   (7, 10, '7', 'Point'),
+	   (8, 7, 'Pass', 'PassFail'),
 	   (8, 8, 'Pass', 'PassFail'),
 	   (8, 9, 'Pass', 'PassFail'),
-	   (8, 10, 'Pass', 'PassFail'),
-	   (7, 11, '3', 'Point'),
-	   (7, 12, '3', 'Point')
+	   (7, 10, '3', 'Point'),
+	   (7, 11, '3', 'Point')
