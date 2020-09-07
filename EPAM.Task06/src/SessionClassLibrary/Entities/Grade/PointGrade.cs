e015e87685746994f1fced7230a428dc642cf669
AnatoliDzenisenka <notif@tut.by>
2020-09-07 20:40:32 +0300

    Amend class

--- EPAM.Task06/src/SessionClassLibrary/Entities/Grade/PointGrade.cs
+++ EPAM.Task06/src/SessionClassLibrary/Entities/Grade/PointGrade.cs
@@ -6,9 +6,70 @@ namespace SessionClassLibrary.Entities.Grade
 {
     public class PointGrade : Grade
     {
-        public override int Id { get; set; }
-        public override int AssessmentId { get; set; }
-        public override int StudentId { get; set; }
-        public int Value { get; set; }
+        private int _id;
+        public override int Id
+        {
+            get => _id;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("ID should be positive.");
+                _id = value;
+            }
+        }
+
+        private int _assessmentId;
+        public override int AssessmentId
+        {
+            get => _assessmentId;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Assessment ID should be positive.");
+            }
+        }
+
+        private int _studentId;
+        public override int StudentId
+        {
+            get => _studentId;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("Student ID should be positive");
+                _studentId = value;
+            }
+        }
+
+        private int _value;
+        public int Value 
+        { 
+            get => _value; 
+            set
+            {
+                if (value < 0 || value > 10)
+                    throw new ArgumentOutOfRangeException("The value should be within 0 - 10.");
+                _value = value;
+            } 
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            return obj is PointGrade grade &&
+                   grade.Id == Id &&
+                   grade.StudentId == StudentId &&
+                   grade.AssessmentId == AssessmentId &&
+                   grade.Value == Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, AssessmentId, StudentId, Value);
+        }
     }
 }
